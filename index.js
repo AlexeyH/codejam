e261b1a658cbbb600b503011331b0ec1e92a9b95
Aliaksei Kharlanau <mailkeeper@tut.by>
2019-11-03 00:46:07 +0300

    init: add minimal functionality app

---
+++ index.js
@@ -0,0 +1,142 @@
+let keyboardKeys = [
+  ['Backquote', 'ё', 'Ё','`','~'],
+  ['Digit1','1','!','1','!'],
+  ['Digit2','2','"','2','@'],
+  ['Digit3','3','№','3','#'],
+  ['Digit4','4',';','4','$'],
+  ['Digit5','5','%','5','%'],
+  ['Digit6','6',':','6','^'],
+  ['Digit7','7','?','7','&'],
+  ['Digit8','8','*','8','*'],
+  ['Digit9','9','(','9','('],
+  ['Digit0','0',')','0',')'],
+  ['Minus','-','_','-','_'],
+  ['Equal','=','+','=','+'],
+  ['Backspace','Backspace','Backspace','Backspace','Backspace'],
+
+  ['Tab','Tab','Tab','Tab','Tab'],
+  ['KeyQ','й','Й','q','Q'],
+  ['KeyW','ц','Ц','w','W'],
+  ['KeyE','у','У','e','E'],
+  ['KeyR','к','К','r','R'],
+  ['KeyT','е','Е','t','T'],
+  ['KeyY','н','Н','y','Y'],
+  ['KeyU','г','Г','u','U'],
+  ['KeyI','ш','Ш','i','I'],
+  ['KeyO','щ','Щ','o','O'],
+  ['KeyP','з','З','p','P'],
+  ['BracketLeft','х','Х','[','{'],
+  ['BracketRight','ъ','Ъ',']','}'],
+  ['Delete','Del','Del','Del','Del'],
+
+  ['CapsLock','Caps Lock','Caps Lock','Caps Lock','Caps Lock'],
+  ['KeyA','ф','Ф','a','A'],
+  ['KeyS','ы','Ы','s','S'],
+  ['KeyD','в','В','d','D'],
+  ['KeyF','а','А','f','F'],
+  ['KeyG','п','П','g','G'],
+  ['KeyH','р','Р','h','H'],
+  ['KeyJ','о','О','j','J'],
+  ['KeyK','л','Л','k','K'],
+  ['KeyL','д','Д','l','L'],
+  ['Semicolon','ж','Ж',';',':'],
+  ['Quote','э','Э','\'','\"'],
+  ['Enter','Enter','Enter','Enter','Enter'],
+
+  ['ShiftLeft','Shift','Shift','Shift','Shift'],
+  ['Backslash','\\','\\','\\','\\'],
+  ['KeyZ','я','Я','z','Z'],
+  ['KeyX','ч','Ч','x','X'],
+  ['KeyC','с','С','c','C'],
+  ['KeyV','м','М','v','V'],
+  ['KeyB','и','И','b','B'],
+  ['KeyN','т','Т','n','N'],
+  ['KeyM','ь','Ь','m','M'],
+  ['Comma','б','Б',',','<'],
+  ['Period','ю','Ю','.','>'],
+  ['Slash','/',',','/','?'],
+  ['ArrowUp','^','^','^','^'],
+  ['ShiftRight','Shift','Shift','Shift','Shift'],
+
+  ['ControlLeft','Ctrl','Ctrl','Ctrl','Ctrl'],
+  ['MetaLeft','Win','Win','Win','Win'],
+  ['AltLeft','Alt','Alt','Alt','Alt'],
+  ['Space','','','',''],
+  ['AltRight','Alt','Alt','Alt','Alt'],
+  ['ControlRight','Ctrl','Ctrl','Ctrl','Ctrl'],
+  ['ArrowLeft','<','<','<','<'],
+  ['ArrowDown','^','^','^','^'],
+  ['ArrowRight','>','>','>','>']
+];
+
+let currentKeyboardLanguage = 1; // 1: russian, 3: english - according to indexes of keyboardKeys array
+let counter = 0;
+
+for (counter = keyboardKeys.length - 1; counter >= 0; counter--) {
+  let div = document.createElement('div');
+  div.className = "key";
+  div.id = keyboardKeys[counter][0];
+  let innerSpan = document.createElement('span');
+  innerSpan.className = "innerSpan";
+  innerSpan.innerHTML = keyboardKeys[counter][1];
+  div.innerHTML = innerSpan.outerHTML;
+  document.body.prepend(div);
+}
+
+let textarea = document.createElement('textarea');
+textarea.className = "textarea";
+textarea.id = "textarea";
+document.body.prepend(textarea);
+
+function shiftKeyboard(isShiftPressed) {
+  if (isShiftPressed) {
+    currentKeyboardLanguage += 1;
+  } else {
+    currentKeyboardLanguage -= 1;
+  }
+  let elements = document.querySelectorAll('span');
+  for (counter = 0; counter < keyboardKeys.length - 11 - 1; counter++) {
+    elements[counter].innerHTML = keyboardKeys[counter][currentKeyboardLanguage];
+  }
+}
+
+function changeKeyboardLanguage() {
+  if (currentKeyboardLanguage == 1) {
+    currentKeyboardLanguage = 3;
+  }
+  else {
+    currentKeyboardLanguage = 1;
+  }
+
+  counter = 0;
+  let elements = document.querySelectorAll('span');
+  for (let element of elements) {
+    element.innerHTML = keyboardKeys[counter][currentKeyboardLanguage];
+    counter++;
+  }
+}
+
+function getKeyValue(eventCode) {
+  counter = 0;
+  for (counter = keyboardKeys.length - 1; counter >= 0; counter--) {
+    if (eventCode == keyboardKeys[counter][0])
+      return keyboardKeys[counter][currentKeyboardLanguage];
+  }
+}
+
+document.addEventListener('keydown', 
+  function(event) {
+    event.preventDefault();
+    if (event.altKey && event.shiftKey) {
+      changeKeyboardLanguage();
+    }
+    textarea.value += getKeyValue(event.code);
+    let pressedKey = document.getElementById(event.code);
+    pressedKey.classList.add("pressedKey");
+    setTimeout(
+      function () {
+      pressedKey.classList.remove("pressedKey");
+      }, 300);
+  
+  }
+);
